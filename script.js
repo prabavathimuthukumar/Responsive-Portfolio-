05441bd0f7eef03154e9b75b1ab2356837989374
prabavathimuthukumar <121662730+prabavathimuthukumar@users.noreply.github.com>
2024-12-03 00:14:23 -0800

    Add files via upload

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
 let menuIcon = document.querySelector("#menu-icon");
 let navbar = document.querySelector(".navbar");
-let section = document.querySelector(".section");
-let navLinks = document.querySelector("header nav a");
+let section = document.querySelectorAll(".section");
+let navLinks = document.querySelectorAll("header nav a");
 
 window.onscroll =() =>{
     section.forEach(sec => {
         let top = window.scrollY;
         let offset = sec.offsetTop -150;
-        let height = sec.offsetWeight;
+        let height = sec.offsetHeight;
         let id = sec.getAttribute('id');
 
         if(top >= offset && top < offset + height ){
             navLinks.forEach(link =>{
              link.classList.remove('active');
-             document.querySelector("header nav a")("active")
+             document.querySelector('header nav a [href*=' + id + ']').classlist.add("active");
 
 
         
@@ -23,6 +23,8 @@ window.onscroll =() =>{
     })
 }
 menuIcon.onclick = () =>{
-    menuIcon.classList.toggle('active');
+
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle("active");
 }
 
